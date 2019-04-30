b2e0a95e69ae901afc72c23d33c1177ab15998de
JayanthJake <36790333+JayanthJake@users.noreply.github.com>
2019-04-30 20:27:29 +0530

    Update pop_ups.js

--- pop_ups.js
+++ pop_ups.js
@@ -1,3 +1,6 @@
+/*Written by JayanthJake. Do not claim it as yours. I do not expect anything but you are
+allowed to share and use as long as you credit me*/
+
 //script to open websites at a defined interval
 function OpenWindow(){
   var windows = window.open("https://www.youtube.com", "_blank"); //opens youtube.com
