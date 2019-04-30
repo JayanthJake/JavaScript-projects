a5ccc97e24ef09745ebb37d16f4e8aaa61182144
JayanthJake <jayanthsapiens2.71828@gmail.com>
2019-04-30 19:09:34 +0530

    Simpler explanation of constructors

---
+++ constructor.js
@@ -0,0 +1,11 @@
+// Using my object
+function Maths(pi){
+  this.jayanthpi = pi;
+}
+var math = new Maths(3.141592653589793);
+document.write("Using my new object:" + math.jayanthpi);
+
+
+// Using the Math object
+document.write(" and Using Math object:" + Math.PI);
+
